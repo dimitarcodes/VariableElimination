e2d2b43c0a424ecb32c7f453e852ee092520e248
mechachki <49350921+mechachki@users.noreply.github.com>
2020-03-26 21:32:38 +0100

    final added some safe guards for when unknown variables are queried/part of evidence added 2 more example networks to test with from http://www.bnlearn.com/bnrepository/

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ from variable_elim import VariableElimination
 
 if __name__ == '__main__':
     # The class BayesNet represents a Bayesian network from a .bif file in several variables
-    net = BayesNet('earthquake.bif') # Format and other networks can be found on http://www.bnlearn.com/bnrepository/
+    net = BayesNet('sachs.bif') # Format and other networks can be found on http://www.bnlearn.com/bnrepository/
     
     # These are the variables read from the network that should be used for variable elimination
     # print("Nodes:")
@@ -30,10 +30,10 @@ if __name__ == '__main__':
     ve = VariableElimination(net)
 
     # Set the node to be queried as follows:
-    query = 'Alarm'
+    query = 'Mek'
 
     # The evidence is represented in the following way (can also be empty when there is no evidence): 
-    evidence = {'Burglary' : 'True' }  # {, 'Earthquake' : 'True'}
+    evidence = {'PKC' : 'LOW'  }  # {, 'Earthquake' : 'True'}
 
     # Determine your elimination ordering before you call the run function. The elimination ordering   
     # is either specified by a list or a heuristic function that determines the elimination ordering
@@ -41,10 +41,12 @@ if __name__ == '__main__':
     # ordering can for example be set as follows:
 
     elim_order = net.nodes
-    elim_order.remove(query)
-    for k in evidence.keys():
-        elim_order.remove(k)
-    print('\nFor queried variable ', query, '\nThe elimination order is:', elim_order)
-
-    #Call the variable elimination function for the queried node given the evidence and the elimination ordering as follows:
-    ve.run(query, evidence, elim_order)
+    try:
+        elim_order.remove(query)
+        for k in evidence.keys():
+            elim_order.remove(k)
+        print('\nFor queried variable ', query, '\nThe elimination order is:', elim_order)
+        # Call the variable elimination function for the queried node given the evidence and the elimination ordering as follows:
+        ve.run(query, evidence, elim_order)
+    except:
+        print('query/evidence not in bayesnet')
